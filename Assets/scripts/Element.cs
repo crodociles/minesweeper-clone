cbb1ae94a9e55069a1344290e9c7879f7f1e479e
crodociles <lukefroud86@gmail.com>
2017-12-23 21:04:00 +0000

    Initial commit

---
+++ Assets/scripts/Element.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour {
+    
+    //Is it a mine
+    public bool mine;
+
+    //Different textures
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+	// Use this for initialization
+	void Start () {
+        //Randomly decide if mine
+        mine = Random.value < 0.15;
+
+        // Register in Grid
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+    }
+
+    //Load another texture
+    public void LoadTexture(int adjacentCount)
+    {
+        if (mine)
+        {
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+        }
+    }
+
+    // Is it still covered?
+    public bool IsCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (mine)
+        {
+            //uncover all mines
+            Grid.UncoverMines();
+
+            //gameover
+            print("You lose");
+        }
+        else
+        {
+            //Show number of adjacent mines
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            LoadTexture(Grid.AdjacentMines(x, y));
+
+            //uncover area without mines
+            Grid.FFuncover(x, y, new bool[Grid.w, Grid.h]);
+
+            //Find out if game won
+            if (Grid.IsFinished())
+                print("you win");
+        }
+    }
+}
---
+++ Assets/scripts/Grid.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    public static int w = 10;
+    public static int h = 13;
+    public static Element[,] elements = new Element[w,h];
+
+    public static void UncoverMines()
+    {
+        foreach (Element elem in elements)
+            if (elem.mine)
+                elem.LoadTexture(0);
+    }
+    //Is mine at coords
+    public static bool MineAt(int x, int y)
+    {
+        //Coordinates in range: check for mine
+        if(x>=0 && y >= 0 && x < w && y < h)
+        {
+            return elements[x, y].mine;
+        }
+        return false;
+    }
+
+    public static int AdjacentMines(int x, int y)
+    {
+        int count = 0;
+
+        //count adjacent mines
+        if (MineAt(x, y + 1)) ++count; // top
+        if (MineAt(x + 1, y + 1)) ++count; // top-right
+        if (MineAt(x + 1, y)) ++count; // right
+        if (MineAt(x + 1, y - 1)) ++count; // bottom-right
+        if (MineAt(x, y - 1)) ++count; // bottom
+        if (MineAt(x - 1, y - 1)) ++count; // bottom-left
+        if (MineAt(x - 1, y)) ++count; // left
+        if (MineAt(x - 1, y + 1)) ++count; // top-left
+
+        return count;
+    }
+
+    public static void FFuncover(int x, int y, bool[,] visited)
+    {
+        // Coordinates in Range?
+        if (x >= 0 && y >= 0 && x < w && y < h)
+        {
+            //visited already
+            if (visited[x, y])
+                return;
+
+            // uncover element
+            elements[x, y].LoadTexture(AdjacentMines(x, y));
+
+            // close to a mine? then no more work needed here
+            if (AdjacentMines(x, y) > 0)
+                return;
+
+            //set visited flag
+            visited[x, y] = true;
+
+            //recursion
+            FFuncover(x - 1, y, visited);
+            FFuncover(x + 1, y, visited);
+            FFuncover(x, y - 1, visited);
+            FFuncover(x, y + 1, visited);
+        }
+    }
+
+    public static bool IsFinished()
+    {
+        // Try to find a covered element that is no mine
+        foreach (Element elem in elements)
+            if (elem.IsCovered() && !elem.mine)
+                return false;
+        // There are none => all are mines => game won.
+        return true;
+    }
+}
